34754353bcb7bd96f923ca4858f4e877bc5ed9a2
dejeong <ahjeong921@gmail.com>
2024-09-18 15:30:36 +0900

    Refactor

--- src/Product.java
+++ src/Product.java
@@ -2,29 +2,27 @@ public class Product implements Promotion {
     String name;
     int price;
     int weight;
-    int discount;
 
-    public Product(String name, int price, int weight, int discount) {
+    public Product(String name, int price, int weight) {
         this.name = name;
         this.price = price;
         this.weight = weight;
-        this.discount = discount;
     }
 
-    @Override
-    public int getDiscountAmount() {
-        return discount;
+    public String getName() {
+        return name;
     }
 
-    public int getPriceAfterDiscount() {
-        return price - discount;
+    public int getPrice() {
+        return price;
     }
 
     public int getWeight() {
         return weight;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public int getDiscountAmount() {
+        return 0;
     }
 }
