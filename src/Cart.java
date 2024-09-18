af323804d8a842269b5d74417c41793cbb3f62e5
dejeong <ahjeong921@gmail.com>
2024-09-18 15:56:32 +0900

    Feat : calculateDeliveryCharge

---
+++ src/Cart.java
@@ -0,0 +1,34 @@
+public class Cart {
+    Product[] products;
+
+    public Cart(Product[] products){
+        this.products = products;
+    }
+
+    public int calculateDeliveryCharge(){
+        int totalWeight = 0;
+        int totalPrice = 0;
+
+        for (Product product : products) {
+            totalWeight += product.getWeight();
+            totalPrice += (product.getPrice() - product.getDiscountAmount());
+        }
+
+        int deliveryCharge;
+        if (totalWeight < 3) {
+            deliveryCharge = 1000;
+        } else if (totalWeight < 10) {
+            deliveryCharge = 5000;
+        } else {
+            deliveryCharge = 10000;
+        }
+
+        if (totalPrice >= 100000) {
+            deliveryCharge = 0;
+        } else if (totalPrice >= 30000) {
+            deliveryCharge = Math.max(deliveryCharge - 1000, 0);
+        }
+
+        return deliveryCharge;
+    }
+}
